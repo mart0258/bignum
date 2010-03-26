84e0023e626f16ac559de2c64a8e5be37b1c34aa
unknown <mart0258@.(none)>
2010-03-25 23:56:15 -0400

    Filling operators

--- bignum.cpp
+++ bignum.cpp
@@ -1,8 +1,28 @@
+#include <iostream>
+
+#include <string>
 #include "bignum.h"
 
 
 int main (void)
 {
-	bignum<2> x;
+	bignum<2> x,y,z;
 
+	x.num[0]=0x0001; x.num[1]=1;
+	y.num[0]=0x2; y.num[1]=0;
+	printf ("%x %x\n", x.num[1], x.num[0]);
+	printf ("%x %x\n", y.num[1], y.num[0]);
+	z=x/y;
+
+	//std::cout << (x < y) << std::endl;
+/*
+	printf ("x:%x %x\n", x.num[1], x.num[0]);
+	printf ("y:%x %x\n", y.num[1], y.num[0]);
+	printf ("z:%x %x\n", z.num[1], z.num[0]);
+	/**/
+	
+	std::string s = x.toString();
+
+	std::cout << "result:" << s << std::endl;
+	/**/
 }
--- bignum.h
+++ bignum.h
@@ -1,16 +1,39 @@
 
+#include <stdio.h>
+#include <string>
 template <int S>
 class bignum
 {
 private:
-	unsigned short num[S];
 
 public:
+	unsigned short num[S];
 	bignum()
 	{
 		for (int i=0; i<S; ++i)
 		{
-			num[S]=0;
+			num[i]=0;
+		}
+	}
+
+	bignum(short j)
+	{
+		num[0]=j;
+		for (int i=1; i<S; ++i)
+		{
+			num[i]=0;
+		}
+	}
+
+
+	bignum(int j)
+	{
+
+		num[0]=j;
+		if (S>1) num[1]=j>>16;
+		for (int i=2; i<S; ++i)
+		{
+			num[i]=0;
 		}
 	}
 
@@ -25,7 +48,8 @@ public:
 		return ret;
 	}
 
-	bignum operator + (bignum &rhs)
+
+	bignum operator + (const bignum &rhs) const 
 	{
 		bignum ret;
 		int carry=0;
@@ -36,9 +60,10 @@ public:
 			carry=((int)num[i]+(int)rhs.num[i])>>16; // 16 bits
 			ret.num[i]=v;
 		}
+		return ret; 
 	}
 
-	bignum operator - (bignum &rhs)
+	bignum operator - (const bignum &rhs) const 
 	{
 		bignum ret;
 		int carry=0;
@@ -46,34 +71,336 @@ public:
 		for (int i=0; i<S; ++i)
 		{
 			v=num[i]-rhs.num[i]-carry;
+			carry=0;
 			if (rhs.num[i]>num[i])
 				carry=1;
 			ret.num[i]=v;
 		}
+		return ret; 
 	}
 
-	bignum operator * (bignum &rhs)
+	bignum operator * (const bignum &rhs) const 
 	{
 		bignum ret;
 		int carry=0;
 		unsigned int v;
-		for (int i=0; i<S; ++i)
-		{
-			ret.num[i]=0;
-		}
 
 		for (int i=0; i<S; ++i)
 		{
 			carry=0;
 			for (int j=0; i+j<S; ++j)
 			{
-				v=num[i]*num[j]+carry;
-				carry=(v+ret[i+j])>>16; // 16 bits
-				ret[i+j]+=v;
+				v=num[i]*rhs.num[j]+carry;
+				carry=(v+ret.num[i+j])>>16; // 16 bits
+				ret.num[i+j]+=v;
 			}
 		}
 		return ret;
 	}
+
+	bignum operator / (const bignum &rhs) const 
+	{
+		bignum div = rhs;
+
+		int p=1;
+
+		while (!(div.num[S-1]&0x8000) && (div < *this))
+		{
+			div <<=1;
+			p++;
+			if (p==S*16+1) throw -1;
+		}
+
+		bignum ret;
+		bignum cur(*this);
+
+		if (div<cur)
+		{
+			cur-=div; 
+			ret.num[0]|=1;
+		}
+	
+
+		while (--p>=0)
+		{
+			ret <<=1;
+			if (div<=cur)
+			{
+				cur-=div; 
+				ret.num[0]|=1;
+			}
+			div>>=1;
+		}
+
+		return ret; 
+		//TODO
+	}
+
+	bignum operator % (const bignum &rhs) const 
+	{
+		bignum div = rhs;
+
+		int p=1;
+
+		while (!(div.num[S-1]&0x8000) && (div < *this))
+		{
+			div <<=1;
+			p++;
+			if (p==S*16+1) throw -1;
+		}
+
+		bignum ret;
+		bignum cur(*this);
+
+		if (div<cur)
+		{
+			cur-=div; 
+			ret.num[0]|=1;
+		}
+	
+
+		while (--p>=0)
+		{
+			ret <<=1;
+			if (div<=cur)
+			{
+				cur-=div; 
+				ret.num[0]|=1;
+			}
+			div>>=1;
+		}
+
+		return cur; 
+		//TODO
+	}
+
+	bignum operator << (int rhs) const
+	{
+		bignum ret; 
+		int i=0, j=0; // 16 bits 
+
+		while (i<S)
+		{
+			if (i<rhs/16)
+			{
+				ret.num[i++]=0;
+			}
+			else
+			{
+				if (j<S)
+				{
+					ret.num[i]=num[j];
+				} else
+				{
+					ret.num[i]=0;
+				}
+				++i, ++j;
+			}
+		}
+		rhs%=16; 
+		
+		if (rhs>0)
+		{
+			for (i=S-1; i>=0; --i)
+			{
+				ret.num[i]<<=rhs;
+				if (i>0)
+					ret.num[i]|=ret.num[i-1]>>(16-rhs);
+			}
+		}
+
+		return ret; 
+	}
+
+	bignum operator >> (int rhs) const
+	{
+		bignum ret; 
+		int i=0, j=rhs/16; // 16 bits 
+
+		while (i<S)
+		{
+			if (j<S)
+			{
+				ret.num[i]=num[j];
+			} else
+			{
+				ret.num[i]=0;
+			}
+			++i, ++j;
+		}
+		rhs%=16; 
+		
+		if (rhs>0)
+		{
+			for (i=0; i<S; ++i)
+			{
+				ret.num[i]>>=rhs;
+				if (i<S-1)
+					ret.num[i]|=ret.num[i+1]<<(16-rhs);
+			}
+		}
+
+		return ret; 
+	}
+
+	bool operator < (const bignum &rhs) const
+	{
+		for (int i=S-1; i>=0; i--)
+		{
+			if (num[i]>rhs.num[i])
+				return false;
+			if (num[i]<rhs.num[i])
+				return true;
+		}
+		if (num[0]==rhs.num[0]) return false;
+		return true;
+	}
+
+	bool  operator > (const bignum &rhs) const
+	{
+		for (int i=S-1; i>=0; i--)
+		{
+			if (num[i]<rhs.num[i])
+				return false;
+			if (num[i]>rhs.num[i])
+				return true;
+		}
+		if (num[0]==rhs.num[0]) return false;
+		return true;
+	}
+
+	bool  operator <= (const bignum &rhs) const
+	{
+		for (int i=S-1; i>=0; i--)
+		{
+			if (num[i]>rhs.num[i])
+				return false;
+			if (num[i]<rhs.num[i])
+				return true;
+		}
+		return true;
+	}
+
+	bool operator >= (const bignum &rhs) const
+	{
+		for (int i=S-1; i>=0; i--)
+		{
+			if (num[i]<rhs.num[i])
+				return false;
+			if (num[i]>rhs.num[i])
+				return true;
+		}
+		return true;
+	}
+
+	
+	bool operator == (const bignum &rhs) const
+	{
+		for (int i=S-1; i>=0; i--)
+		{
+			if (num[i]!=rhs.num[i])
+				return false;
+		}
+		return true;
+	}
+
+	bool operator != (const bignum &rhs) const
+	{
+		for (int i=S-1; i>=0; i--)
+		{
+			if (num[i]==rhs.num[i])
+				return true;
+		}
+		return false;
+	}
+
+	std::string toString()
+	{
+		bignum ten;
+		ten.num[0]=10;
+		bignum pl;
+		pl.num[0]=1;
+		std::string ret;
+		bignum cur=*this;
+		bignum div;
+
+		int ex=0;
+		int z=0;
+
+		//printf ("ToString() called\n");
+		do
+		{
+			bignum test = cur / pl;
+			//printf ("%d-", test.num[0]);
+			if (test<ten) break;
+			pl = pl * ten;
+			++ex;
+			//printf ("%d\n", ex);
+		}while (1);
+
+		while (ex-->=0)
+		{
+			div=cur/pl;
+			/*if (div.num[0]==0 && !z)
+			{
+				pl=pl/ten;
+				continue;
+			}*/
+
+			z=1;
+			//printf ("Appending: %d\n", div.num[0]);
+			ret+=('0'+div.num[0]);
+			cur-=div*pl;
+			pl=pl/ten;
+		}
+
+		if (ret.size()==0) ret+='0';
+
+		return ret;
+	}
+
+	bignum &operator += (const bignum &rhs) 
+	{
+		*this = *this + rhs;
+
+		return *this;
+	}
+
+	bignum &operator -= (const bignum &rhs) 
+	{
+		*this = *this - rhs;
+
+		return *this;
+	}
+
+	bignum &operator *= (const bignum &rhs) 
+	{
+		*this = *this * rhs;
+
+		return *this;
+	}
+
+	bignum &operator /= (const bignum &rhs) 
+	{
+		*this = *this / rhs;
+
+		return *this;
+	}
+
+	bignum &operator <<= (int rhs) 
+	{
+		*this = *this << rhs;
+
+		return *this;
+	}
+	bignum &operator >>= (int rhs) 
+	{
+		*this = *this >> rhs;
+
+		return *this;
+	}
+
+
 
 
 };
